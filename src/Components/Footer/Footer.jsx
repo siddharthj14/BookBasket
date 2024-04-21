3a57e134904f218d53379a0d45df04c75de1cc54
Siddharth Jain <siddharthj1405@gmail.com>
2024-04-21 23:35:09 +0530

    Update Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return <div className="footer">
     <div className="footer-logo"></div>
+    <div className="credits">~Made with ❤️ by Siddharth</div>
   </div>;
 };
 
