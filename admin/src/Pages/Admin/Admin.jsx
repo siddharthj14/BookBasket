a49906fc7f88c782a7bab839d7b267b5a652ca81
Siddharth Jain <siddharthj1405@gmail.com>
2025-05-20 23:51:56 +0530

    Add Navbar and Sidebar for Admin part

---
+++ admin/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Admin.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+
+const Admin = () => {
+  return <div clasName="admin">
+    <Sidebar/>
+  </div>;
+};
+
+export default Admin;
