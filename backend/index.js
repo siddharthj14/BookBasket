a8536bc60df9b25f908382133789cdce5b0fcf09
Siddharth Jain <siddharthj1405@gmail.com>
2025-05-20 15:57:40 +0530

    Add dotenv configuration and product model; update MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,15 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://siddharthj1405:FFVFZ4kOScZQPxD1@cluster0.pvxygok.mongodb.net/bookbasket"
-);
-
-app.listen(port, (error) => {
-  if (error) {
-    console.log("Error in starting the server" + error);
-  } else {
-    console.log("Server started on port " + port);
-  }
-});
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.send("Express App is Running on Port " + port);
@@ -47,3 +41,65 @@ app.post("/upload", upload.single("product"), (req, res) => {
   });
 });
 
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+app.post("/add-product", async (req, res) => {
+  const product = new Product({
+    id: req.body.id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  });
+  console.log(product);
+  await product.save();
+  console.log("Product saved successfully");
+  res.json({
+    success: true,
+    message: "Product added successfully",
+    name: req.body.name,
+  });
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log("Error in starting the server" + error);
+  } else {
+    console.log("Server started on port " + port);
+  }
+});
