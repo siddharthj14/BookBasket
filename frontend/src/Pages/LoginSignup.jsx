977b6663dfe13c313785543b3042cc975ff67d17
Siddharth Jain <siddharthj1405@gmail.com>
2025-05-23 00:18:43 +0530

    feat: enhance login/signup component with state management and form handling

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,24 +1,88 @@
-import React from "react";
 import "./CSS/LoginSignup.css";
+import { useState } from "react";
 
 const LoginSignup = () => {
+  const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+    email: "",
+  });
+
+  const handleLogin = async () => {
+    console.log("Login", formData);
+  };
+  const handleSignUp = async () => {
+    console.log("SignUp", formData);
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="loginsignup">
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          {state == "Sign Up" && (
+            <input
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              type="text"
+              placeholder="Your Name"
+            />
+          )}
+          <input
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            type="email"
+            placeholder="Email Address"
+          />
+          <input
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            type="password"
+            placeholder="Password"
+          />
         </div>
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
           <p>By continuing, I agree to the terms of use & privacy policy.</p>
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">
-          Already have an account? <span>Login here</span>
-        </p>
+        <button
+          onClick={() => {
+            state === "Login" ? handleLogin() : handleSignUp();
+          }}
+        >
+          Continue
+        </button>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account?
+            <span
+              onClick={() => {
+                setState("Login");
+              }}
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account?
+            <span
+              onClick={() => {
+                setState("Sign Up");
+              }}
+            >
+              Click here
+            </span>
+          </p>
+        )}
       </div>
     </div>
   );
